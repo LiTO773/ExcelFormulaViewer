9b5ca7244fafe288d6d333c1d5b3e1c7dba9a72c
LiTO773 <devluisp@hotmail.com>
2019-03-05 22:03:55 +0000

    Updates

---
+++ precache-manifest.88cf78a9e9e204bf49b9ad86994d244a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2a8bfabac2734b572cd4",
+    "url": "/static/js/main.2a8bfaba.chunk.js"
+  },
+  {
+    "revision": "9ab79b18f5548a4be9a7",
+    "url": "/static/js/2.9ab79b18.chunk.js"
+  },
+  {
+    "revision": "2a8bfabac2734b572cd4",
+    "url": "/static/css/main.e0ff0793.chunk.css"
+  },
+  {
+    "revision": "29297874b96c0f4d12d30f0f1d406cfd",
+    "url": "/index.html"
+  }
+];
