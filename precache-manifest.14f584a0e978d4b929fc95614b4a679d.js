89b98b6eed1cf0872ea2ac042770742b59a1077d
LiTO773 <devluisp@hotmail.com>
2019-03-05 22:09:36 +0000

    Updates

---
+++ precache-manifest.14f584a0e978d4b929fc95614b4a679d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36e96549bf6fdac4f816",
+    "url": "/ExcelFormulaViewer/static/js/runtime~main.36e96549.js"
+  },
+  {
+    "revision": "e3bc025cc4a6329f7435",
+    "url": "/ExcelFormulaViewer/static/js/main.e3bc025c.chunk.js"
+  },
+  {
+    "revision": "9ab79b18f5548a4be9a7",
+    "url": "/ExcelFormulaViewer/static/js/2.9ab79b18.chunk.js"
+  },
+  {
+    "revision": "e3bc025cc4a6329f7435",
+    "url": "/ExcelFormulaViewer/static/css/main.e0ff0793.chunk.css"
+  },
+  {
+    "revision": "57f2328ae8383715b4c3e750cc95b49b",
+    "url": "/ExcelFormulaViewer/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.88cf78a9e9e204bf49b9ad86994d244a.js"
+  "/ExcelFormulaViewer/precache-manifest.14f584a0e978d4b929fc95614b4a679d.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/ExcelFormulaViewer/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
