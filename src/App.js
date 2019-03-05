22a3814bc54ae6199bc24c092f4feaac5cff44d9
LiTO773 <devluisp@hotmail.com>
2019-03-05 22:24:30 +0000

    Site is now deployed in Netlify Added a title to the webpage

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   render () {
     return (
       <div id='App'>
-      <h1>Hi</h1>
+        <h1>Excel Formula Viewer</h1>
         <input onChange={this.handleInput} value={this.state.formula} />
         <br/>
         <MathJax.Context>
